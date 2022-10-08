ef44f7e19fab90ed3db355f12cc60eba8b1ae6e7
Adam Demasi <adam@hbang.com.au>
2022-10-09 01:02:21 +1030

    Switch to using 7-Zip SFX for the installer

--- activex/build/buildcab.cmd
+++ activex/build/buildcab.cmd
@@ -2,7 +2,7 @@
 cd %~dp0
 set build=%1
 if "%build%" == "" set build=Debug
-path %PATH%;%ProgramFiles%\Resource Hacker
+path %PATH%;%ProgramFiles%\Resource Hacker;%ProgramFiles%\7-Zip
 
 :: Copy files
 copy ..\%build%\LegacyUpdateOCX.dll LegacyUpdate.dll
@@ -16,7 +16,10 @@ if not exist vcredist_x86.exe copy "%redist%\vcredist_x86\vcredist_x86.exe" vcre
 :: del setup.rpt
 
 :: exe for manual installation
-iexpress /n /q LegacyUpdate.sed
+del LegacyUpdate.7z
+7z a -mx6 -- LegacyUpdate.7z LegacyUpdate.inf LegacyUpdate.dll vcredist_x86.exe
+copy /b 7zSD.sfx + sfx.txt + LegacyUpdate.7z LegacyUpdate.exe
+
 ResourceHacker -open versioninfo.rc -save versioninfo.res -action compile -log CONSOLE
 ResourceHacker -script resources.txt
 del versioninfo.res
