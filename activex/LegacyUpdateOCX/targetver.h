9aaa8f71ce1f629545df4f2913a9f58f1626443d
Adam Demasi <adam@hbang.com.au>
2022-10-13 14:51:25 +1030

    Clean up build a bit; fix VC17 not liking Win2k WINVER

--- activex/LegacyUpdateOCX/targetver.h
+++ activex/LegacyUpdateOCX/targetver.h
@@ -1,5 +1,11 @@
 #pragma once
 
-// Windows 2000 RTM
-#define WINVER 0x0500
-#define _WIN32_WINNT 0x0500
+#if _MSC_VER > 1599
+	// VC17: Windows XP
+	#define WINVER 0x0501
+	#define _WIN32_WINNT 0x0501
+#else
+	// VC08: Windows 2000 RTM
+	#define WINVER 0x0500
+	#define _WIN32_WINNT 0x0500
+#endif
