c3ed01f9568660141e3a6f6144a46467cc3beae3
Adam Demasi <adam@hbang.com.au>
2022-11-11 10:27:30 +1030

    Not sure why Resource.h was being ignored

---
+++ activex/Resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LegacyUpdateOCX.rc
+//
+#define IDS_LEGACYUPDATEOCX             1
+#define IDS_LEGACYUPDATEOCX_PPG         2
+#define IDS_LEGACYUPDATEOCX_PPG_CAPTION 200
+#define IDI_ICON1                       201
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        202
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         201
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
