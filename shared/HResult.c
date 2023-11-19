2bcaaca1bfe652e070472d8130555e01bc7e1105
Adam Demasi <adam@hbang.com.au>
2023-11-19 15:51:16 +1030

    Link the plugin against msvcrt.dll to reduce file size

--- shared/HResult.c
+++ shared/HResult.c
@@ -1,5 +1,4 @@
 #include <windows.h>
-#include <strsafe.h>
 #include "WUErrors.h"
 
 EXTERN_C LPWSTR GetMessageForHresult(HRESULT hr) {
@@ -7,14 +6,14 @@ EXTERN_C LPWSTR GetMessageForHresult(HRESULT hr) {
 	for (int i = 0; WUErrorMessages[i].hr != 0; i++) {
 		if (WUErrorMessages[i].hr == hr) {
 			message = (LPWSTR)LocalAlloc(LPTR, 4096 * sizeof(WCHAR));
-			StringCchCopy(message, 4096, WUErrorMessages[i].message);
+			lstrcpy(message, WUErrorMessages[i].message);
 			return message;
 		}
 	}
 
 	if (FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&message, 0, NULL) == 0) {
 		message = (LPWSTR)LocalAlloc(LPTR, 1024 * sizeof(WCHAR));
-		StringCchPrintf(message, 1024, L"Error 0x%08x", hr);
+		wsprintf(message, L"Error 0x%08x", hr);
 	}
 
 	return message;
