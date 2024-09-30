2ee33d9c53e3062453dca2dcdb9365c31221123c
Adam Demasi <adam@hbang.com.au>
2024-09-30 16:56:52 +0930

    Make hex error codes uppercase, remove title lines in braces

--- shared/HResult.c
+++ shared/HResult.c
@@ -15,13 +15,22 @@ STDAPI_(LPWSTR) GetMessageForHresult(HRESULT hr) {
 
 	if (FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&message, 0, NULL) == 0) {
 		message = (LPWSTR)LocalAlloc(LPTR, 1024 * sizeof(WCHAR));
-		wsprintf(message, L"Error 0x%08x", hr);
+		wsprintf(message, L"Error 0x%08X", hr);
+		return message;
 	}
 
 	// Truncate trailing \r\n if any
 	int len = lstrlen(message);
 	if (len >= 2 && message[len - 2] == '\r' && message[len - 1] == '\n') {
 		message[len - 2] = 0;
+	}
+
+	// Remove title part in braces
+	if (len > 1 && message[0] == '{') {
+		LPWSTR closeBrace = wcsstr(message, L"}\r\n");
+		if (closeBrace) {
+			message = closeBrace + 3;
+		}
 	}
 
 	return message;
