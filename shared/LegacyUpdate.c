224b8b9c297e389146cfd6a89858411d8d576740
Adam Demasi <adam@hbang.com.au>
2024-10-04 23:42:02 +0930

    Stick to getting program files dir from the registry

--- shared/LegacyUpdate.c
+++ shared/LegacyUpdate.c
@@ -2,12 +2,6 @@
 #include <windows.h>
 #include "Registry.h"
 
-#if _WIN64
-	#define ProgramFilesEnv L"%ProgramFilesW6432%"
-#else
-	#define ProgramFilesEnv L"%ProgramFiles%"
-#endif
-
 static LPWSTR _installPath;
 
 EXTERN_C HRESULT GetInstallPath(LPWSTR *path) {
@@ -23,10 +17,12 @@ EXTERN_C HRESULT GetInstallPath(LPWSTR *path) {
 		}
 
 		// Do our best to guess where it should be
-		hr = S_OK;
-		_installPath = (LPWSTR)LocalAlloc(LPTR, MAX_PATH * sizeof(WCHAR));
-		if (ExpandEnvironmentStrings(ProgramFilesEnv L"\\Legacy Update", _installPath, MAX_PATH) == 0) {
-			hr = HRESULT_FROM_WIN32(GetLastError());
+		LPWSTR programFiles;
+		hr = GetRegistryString(HKEY_LOCAL_MACHINE, L"Software\\Microsoft\\Windows\\CurrentVersion", L"ProgramFilesDir", KEY_WOW64_64KEY, &programFiles, &size);
+		if (SUCCEEDED(hr)) {
+			_installPath = (LPWSTR)LocalAlloc(LPTR, MAX_PATH * sizeof(WCHAR));
+			wsprintf(_installPath, L"%ls\\Legacy Update", programFiles);
+			LocalFree(programFiles);
 		}
 	}
 
