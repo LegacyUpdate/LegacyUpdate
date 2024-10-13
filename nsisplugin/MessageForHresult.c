9a097ac6643c3896ad1c28eea44771bea8aa39a7
Adam Demasi <adam@hbang.com.au>
2024-10-13 12:35:24 +1030

    Detect invalid inputs to MessageForHresult

--- nsisplugin/MessageForHresult.c
+++ nsisplugin/MessageForHresult.c
@@ -5,7 +5,18 @@
 PLUGIN_METHOD(MessageForHresult) {
 	PLUGIN_INIT();
 
-	HRESULT hr = popint();
+	LPWSTR str;
+	popstring(str);
+
+	// Handle non-numeric inputs
+	for (int i = 0; str[i] != L'\0'; i++) {
+		if (str[i] < L'0' || str[i] > L'9') {
+			pushstring(L"Unknown error");
+			return;
+		}
+	}
+
+	HRESULT hr = nsishelper_str_to_ptr(str);
 	LPWSTR message = GetMessageForHresult(hr);
 	pushstring(message);
 	LocalFree(message);
