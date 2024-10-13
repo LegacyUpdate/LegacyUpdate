5fd4dff7b8fc36d3cefb57543552ef286d5004d6
Adam Demasi <adam@hbang.com.au>
2024-10-14 01:38:31 +1030

    Instead of doing funny stuff, just assume 0 is unexpected

--- nsisplugin/MessageForHresult.c
+++ nsisplugin/MessageForHresult.c
@@ -5,18 +5,12 @@
 PLUGIN_METHOD(MessageForHresult) {
 	PLUGIN_INIT();
 
-	LPWSTR str;
-	popstring(str);
-
-	// Handle non-numeric inputs
-	for (int i = 0; str[i] != L'\0'; i++) {
-		if (str[i] < L'0' || str[i] > L'9') {
-			pushstring(L"Unknown error");
-			return;
-		}
+	HRESULT hr = popint();
+	if (hr == 0) {
+		pushstring(L"Unknown error");
+		return;
 	}
 
-	HRESULT hr = nsishelper_str_to_ptr(str);
 	LPWSTR message = GetMessageForHresult(hr);
 	pushstring(message);
 	LocalFree(message);
