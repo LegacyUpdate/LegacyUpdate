92e9514723e7d55b9f932fed98170c2a4454c3c9
Adam Demasi <773309+kirb@users.noreply.github.com>
2024-12-12 00:04:51 +1030

    Shut down on Vista+ using InitiateShutdown()
    This allows us to pass `SHUTDOWN_INSTALL_UPDATES`, which seems pretty important.

--- LegacyUpdate/Utils.cpp
+++ LegacyUpdate/Utils.cpp
@@ -8,10 +8,7 @@
 #pragma comment(lib, "version.lib")
 #pragma comment(lib, "advapi32.lib")
 
-// Defined as being Vista+, older versions ignore the flag.
-#ifndef EWX_RESTARTAPPS
-#define EWX_RESTARTAPPS 0x00000040
-#endif
+typedef DWORD (WINAPI *_InitiateShutdownW)(LPWSTR lpMachineName, LPWSTR lpMessage, DWORD dwGracePeriod, DWORD dwShutdownFlags, DWORD dwReason);
 
 static BOOL _loadedProductName = FALSE;
 static CComVariant _productName;
@@ -22,7 +19,8 @@ HRESULT GetOSProductName(LPVARIANT productName) {
 		VariantInit(&_productName);
 
 		if (IsWinXP2003()) {
-			// If this is Windows Storage Server 2003, load the brand string from wssbrand.dll
+			// If this is Windows Storage Server 2003, load the brand string from wssbrand.dll. On this edition, the brand
+			// string from WMI remains "Microsoft Windows Server 2003".
 			HMODULE wssbrand = LoadLibrary(L"wssbrand.dll");
 			if (wssbrand) {
 				WCHAR brand[256];
@@ -49,39 +47,58 @@ HRESULT GetOSProductName(LPVARIANT productName) {
 HRESULT Reboot() {
 	HRESULT hr = E_FAIL;
 
-	// Make sure we have permission to shut down.
+	// Make sure we have permission to shut down
 	HANDLE token;
-	if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token)) {
-		LUID shutdownLuid;
-		if (LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &shutdownLuid) != 0) {
-			// Ask the system nicely to give us shutdown privilege.
-			TOKEN_PRIVILEGES privileges;
-			privileges.PrivilegeCount = 1;
-			privileges.Privileges[0].Luid = shutdownLuid;
-			privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-			if (!AdjustTokenPrivileges(token, FALSE, &privileges, 0, NULL, NULL)) {
-				hr = AtlHresultFromLastError();
-				TRACE("AdjustTokenPrivileges() failed: %ls\n", GetMessageForHresult(hr));
-			}
-		}
-	} else {
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token)) {
 		hr = AtlHresultFromLastError();
 		TRACE("OpenProcessToken() failed: %ls\n", GetMessageForHresult(hr));
+		goto end;
 	}
 
-	// Reboot with reason "Operating System: Security fix (Unplanned)"
-	if (!InitiateSystemShutdownEx(NULL, NULL, 0, FALSE, TRUE, SHTDN_REASON_MAJOR_OPERATINGSYSTEM | SHTDN_REASON_MINOR_SECURITYFIX)) {
+	LUID shutdownLuid;
+	if (!LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &shutdownLuid) != 0) {
 		hr = AtlHresultFromLastError();
-		TRACE("InitiateSystemShutdownExW() failed: %ls\n", GetMessageForHresult(hr));
+		TRACE("LookupPrivilegeValue() failed: %ls\n", GetMessageForHresult(hr));
+		goto end;
+	}
 
-		// Try ExitWindowsEx instead
-		// Win2k: Use ExitWindowsEx, which is only guaranteed to work for the current logged in user
-		if (!ExitWindowsEx(EWX_REBOOT | EWX_RESTARTAPPS | EWX_FORCEIFHUNG, SHTDN_REASON_MAJOR_OPERATINGSYSTEM | SHTDN_REASON_MINOR_SECURITYFIX)) {
+	// Ask the system nicely to give us shutdown privilege
+	TOKEN_PRIVILEGES privileges;
+	privileges.PrivilegeCount = 1;
+	privileges.Privileges[0].Luid = shutdownLuid;
+	privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	if (!AdjustTokenPrivileges(token, FALSE, &privileges, 0, NULL, NULL)) {
+		hr = AtlHresultFromLastError();
+		TRACE("AdjustTokenPrivileges() failed: %ls\n", GetMessageForHresult(hr));
+		goto end;
+	}
+
+	// Reboot with reason "Operating System: Security fix (Unplanned)", ensuring to install updates.
+	// Try InitiateShutdown first (Vista+)
+	_InitiateShutdownW $InitiateShutdownW = (_InitiateShutdownW)GetProcAddress(GetModuleHandle(L"advapi32.dll"), "InitiateShutdownW");
+	if ($InitiateShutdownW) {
+		hr = HRESULT_FROM_WIN32($InitiateShutdownW(NULL, NULL, 0, SHUTDOWN_RESTART | SHUTDOWN_INSTALL_UPDATES, SHTDN_REASON_MAJOR_OPERATINGSYSTEM | SHTDN_REASON_MINOR_SECURITYFIX));
+	}
+
+	// Try InitiateSystemShutdownEx (2k/XP)
+	if (!SUCCEEDED(hr)) {
+		TRACE("InitiateShutdown() failed: %ls\n", GetMessageForHresult(hr));
+
+		if (InitiateSystemShutdownEx(NULL, NULL, 0, FALSE, TRUE, SHTDN_REASON_MAJOR_OPERATINGSYSTEM | SHTDN_REASON_MINOR_SECURITYFIX) == 0) {
 			hr = AtlHresultFromLastError();
-			TRACE("ExitWindowsEx() failed: %ls\n", GetMessageForHresult(hr));
+			TRACE("InitiateSystemShutdownExW() failed: %ls\n", GetMessageForHresult(hr));
 		}
-	} else {
-		hr = S_OK;
+	}
+
+	// Last-ditch attempt ExitWindowsEx (only guaranteed to work for the current logged in user)
+	if (!ExitWindowsEx(EWX_REBOOT | EWX_FORCEIFHUNG, SHTDN_REASON_MAJOR_OPERATINGSYSTEM | SHTDN_REASON_MINOR_SECURITYFIX)) {
+		hr = AtlHresultFromLastError();
+		TRACE("ExitWindowsEx() failed: %ls\n", GetMessageForHresult(hr));
+	}
+
+end:
+	if (token) {
+		CloseHandle(token);
 	}
 
 	return hr;
