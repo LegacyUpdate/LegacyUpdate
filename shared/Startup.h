ef843e03b9f5b5ffc64733ac5b81cb667fc63555
Adam Demasi <adam@hbang.com.au>
2024-12-28 23:42:55 +1030

    Don't trust %SystemRoot% to be safe

--- shared/Startup.h
+++ shared/Startup.h
@@ -8,18 +8,24 @@ typedef BOOL (WINAPI *_SetDllDirectoryW)(LPWSTR);
 static inline void HardenDllSearchPaths() {
 	// Try our best to secure DLL search paths to just system32
 	WCHAR path[MAX_PATH];
-	ExpandEnvironmentStrings(L"%SystemRoot%\\System32\\kernel32.dll", path, ARRAYSIZE(path));
-	HMODULE kernel32 = GetModuleHandle(path);
+
+	// Reset %windir% and %SystemRoot%
+	GetWindowsDirectory(path, ARRAYSIZE(path));
+	SetEnvironmentVariable(L"WINDIR", path);
+	SetEnvironmentVariable(L"SystemRoot", path);
 
 	// Reset %PATH% to just system32
-	ExpandEnvironmentStrings(L"%SystemRoot%\\System32", path, ARRAYSIZE(path));
+	ZeroMemory(path, sizeof(path));
+	GetSystemDirectory(path, ARRAYSIZE(path));
 	SetEnvironmentVariable(L"PATH", path);
 
-	// Windows Vista SP2+/7 SP1+ with KB2533623
+	wcscat_s(path, ARRAYSIZE(path), L"\\kernel32.dll");
+	HMODULE kernel32 = GetModuleHandle(path);
 	if (!kernel32) {
 		return;
 	}
 
+	// Windows Vista SP2+/7 SP1+ with KB2533623
 	_SetDefaultDllDirectories $SetDefaultDllDirectories = (_SetDefaultDllDirectories)GetProcAddress(kernel32, "SetDefaultDllDirectories");
 	if ($SetDefaultDllDirectories) {
 		$SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_SYSTEM32);
