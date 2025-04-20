551837a24de6fd30d3b5f98cf14dec7bb26120d8
Adam Demasi <adam@hbang.com.au>
2025-04-20 12:04:21 +0930

    Enable -Werror -Wpedantic

--- shared/Trace.h
+++ shared/Trace.h
@@ -6,7 +6,7 @@
 	// Yeah, sue me lol
 	#define TRACE(...) { \
 		LPWSTR __traceMsg = (LPWSTR)LocalAlloc(LPTR, 4096 * sizeof(WCHAR)); \
-		wsprintf(__traceMsg, L"%hs(%d): %hs: ", __FILE__, __LINE__, __FUNCTION__); \
+		wsprintf(__traceMsg, L"%hs(%d): %hs: ", __FILE__, __LINE__, __func__); \
 		wsprintf(__traceMsg + wcslen(__traceMsg), __VA_ARGS__); \
 		MessageBox(NULL, __traceMsg, L"Debug", MB_OK); \
 		LocalFree(__traceMsg); \
