LegacyUpdate/LegacyUpdate

commit 771c0436d98706896ca769cc68a24d5c743176c6

Get NT4 build working again

setup/RunOnce.nsh
@@ -77,6 +77,7 @@ Function CleanUpRunOnceFinal
 	RMDir /r /REBOOTOK "$WINDIR\Windows Update Setup Files"
 FunctionEnd
 
+!if ${NT4} == 0
 Function CopyLauncher
 	${If} ${IsNativeAMD64}
 		File /ONAME=LegacyUpdate.exe "..\launcher\obj\LegacyUpdate64.exe"
@@ -84,11 +85,13 @@ Function CopyLauncher
 		File /ONAME=LegacyUpdate.exe "..\launcher\obj\LegacyUpdate32.exe"
 	${EndIf}
 FunctionEnd
+!endif
 
 Var /GLOBAL RunOnce.UseFallback
 
 Function PrepareRunOnce
 	${If} ${RebootFlag}
+		!if ${NT4} == 0
 		${IfNot} ${IsRunOnce}
 			; Copy to runonce path to ensure installer is accessible by the temp user
 			CreateDirectory "${RUNONCEDIR}"
@@ -99,6 +102,7 @@ Function PrepareRunOnce
 			; Remove mark of the web to prevent "Open File - Security Warning" dialog
 			System::Call '${DeleteFile}("${RUNONCEDIR}\LegacyUpdateSetup.exe:Zone.Identifier")'
 		${EndIf}
+		!endif
 
 		${If} $RunOnce.UseFallback == 1
 			WriteRegStr HKLM "${REGPATH_RUNONCE}" "LegacyUpdateRunOnce" '"${RUNONCEDIR}\LegacyUpdateSetup.exe" /runonce'
@@ -106,7 +110,13 @@ Function PrepareRunOnce
 			; Somewhat documented in KB939857:
 			; https://web.archive.org/web/20090723061647/http://support.microsoft.com/kb/939857
 			; See also Wine winternl.h
-			!insertmacro RunOnceOverwriteReg Str   HKLM "${REGPATH_SETUP}" "CmdLine" '"${RUNONCEDIR}\LegacyUpdate.exe" /runonce'
+			!if ${NT4} == 1
+				StrCpy $0 "${RUNONCEDIR}\LegacyUpdateSetup.exe"
+			!else
+				StrCpy $0 "${RUNONCEDIR}\LegacyUpdate.exe"
+			!endif
+
+			!insertmacro RunOnceOverwriteReg Str   HKLM "${REGPATH_SETUP}" "CmdLine" '"$0" /runonce'
 			!insertmacro RunOnceOverwriteReg Dword HKLM "${REGPATH_SETUP}" "SetupType" ${SETUP_TYPE_NOREBOOT}
 			WriteRegDword HKLM "${REGPATH_SETUP}" "SetupShutdownRequired" ${SETUP_SHUTDOWN_REBOOT}
 		${EndIf}

.gitignore
@@ -120,10 +120,3 @@ ServiceFabricBackup/
 *.msix
 *.msm
 *.msp
-
-# Patches we do want to commit
-!setup/patches/**/*.exe
-!setup/patches/**/*.dll
-!setup/patches/**/*.cab
-!setup/patches/**/*.msi
-!setup/patches/**/*.msp

setup/Constants.nsh
@@ -20,7 +20,15 @@
 !define NSIS_TARGET        "${NSIS_CPU}-${NSIS_CHARSET}"
 
 ; Version
-!getdllversion "..\${VSBUILD32}\LegacyUpdate.dll" DLLVersion_
+!if ${NT4} == 1
+	!define DLLVersion_1 1
+	!define DLLVersion_2 11
+	!define DLLVersion_3 1
+	!define DLLVersion_4 0
+!else
+	!getdllversion "..\${VSBUILD32}\LegacyUpdate.dll" DLLVersion_
+!endif
+
 !define LONGVERSION        "${DLLVersion_1}.${DLLVersion_2}.${DLLVersion_3}.${DLLVersion_4}"
 !define VERSION            "${LONGVERSION}"
 

setup/Download2KXP.nsh
@@ -5,7 +5,6 @@
 !insertmacro NeedsSPHandler "2003SP2" "WinXP2003" 1
 
 !insertmacro NeedsFileVersionHandler "W2KUR1" "kernel32.dll" "5.00.2195.7006"
-!insertmacro NeedsFileVersionHandler "IE6"    "mshtml.dll"   "6.0.2600.0"
 
 !insertmacro PatchHandler "W2KSP4"  "Windows 2000 $(SP) 4"      ${PATCH_FLAGS_SHORT} ""
 !insertmacro PatchHandler "W2KUR1"  "$(SectionW2KUR1)"          ${PATCH_FLAGS_LONG}  ""
@@ -13,38 +12,6 @@
 !insertmacro PatchHandler "XPSP3"   "Windows XP $(SP) 3"        ${PATCH_FLAGS_LONG}  ""
 !insertmacro PatchHandler "2003SP2" "Windows XP $(P64)/$(SRV) 2003 $(SP) 2" ${PATCH_FLAGS_LONG} ""
 !insertmacro PatchHandler "XPESP3"  "Windows XP $(EMB) $(SP) 3" ${PATCH_FLAGS_LONG}  ""
-
-!macro DownloadIE ver title
-	${If} ${NeedsPatch} IE${ver}
-		StrCpy $Patch.Key   "IE${ver}"
-		StrCpy $Patch.File  "ie${ver}setup.exe"
-		StrCpy $Patch.Title "${title} $(Setup)"
-		Call -PatchHandler
-
-		${IfNot} ${FileExists} "$PLUGINSDIR\IE${ver}\ie${ver}setup.exe"
-			${DetailPrint} "$(Downloading)${title}..."
-			!insertmacro ExecWithErrorHandling '${title}' '"$PLUGINSDIR\IE${ver}\ie${ver}setup.exe" /c:"ie${ver}wzd.exe /q /d /s:""#e"""'
-		${EndIf}
-	${EndIf}
-!macroend
-
-!macro InstallIE ver title
-	${If} ${NeedsPatch} IE${ver}
-		Call DownloadIE${ver}
-		${DetailPrint} "$(Installing)${title}..."
-		StrCpy $RunOnce.UseFallback 1
-		!insertmacro ExecWithErrorHandling '${title}' '"$PLUGINSDIR\IE${ver}\ie${ver}setup.exe" /c:"ie${ver}wzd.exe /q /r:n /s:""#e"""'
-		RMDir /r /REBOOTOK "$WINDIR\Windows Update Setup Files"
-	${EndIf}
-!macroend
-
-Function DownloadIE6
-	!insertmacro DownloadIE 6 "$(IE) 6 $(SP) 1"
-FunctionEnd
-
-Function InstallIE6
-	!insertmacro InstallIE 6 "$(IE) 6 $(SP) 1"
-FunctionEnd
 
 Function FixW2KUR1
 	; Fix idling on multi-CPU systems when Update Rollup 1 is installed

setup/DownloadIE.nsh
@@ -1,0 +1,33 @@
+!insertmacro NeedsFileVersionHandler "IE6" "mshtml.dll" "6.0.2600.0"
+
+!macro DownloadIE ver title
+	${If} ${NeedsPatch} IE${ver}
+		StrCpy $Patch.Key   "IE${ver}"
+		StrCpy $Patch.File  "ie${ver}setup.exe"
+		StrCpy $Patch.Title "${title} $(Setup)"
+		Call -PatchHandler
+
+		${IfNot} ${FileExists} "$PLUGINSDIR\IE${ver}\ie${ver}setup.exe"
+			${DetailPrint} "$(Downloading)${title}..."
+			!insertmacro ExecWithErrorHandling '${title}' '"$PLUGINSDIR\IE${ver}\ie${ver}setup.exe" /c:"ie${ver}wzd.exe /q /d /s:""#e"""'
+		${EndIf}
+	${EndIf}
+!macroend
+
+!macro InstallIE ver title
+	${If} ${NeedsPatch} IE${ver}
+		Call DownloadIE${ver}
+		${DetailPrint} "$(Installing)${title}..."
+		StrCpy $RunOnce.UseFallback 1
+		!insertmacro ExecWithErrorHandling '${title}' '"$PLUGINSDIR\IE${ver}\ie${ver}setup.exe" /c:"ie${ver}wzd.exe /q /r:n /s:""#e"""'
+		RMDir /r /REBOOTOK "$WINDIR\Windows Update Setup Files"
+	${EndIf}
+!macroend
+
+Function DownloadIE6
+	!insertmacro DownloadIE 6 "$(IE) 6 $(SP) 1"
+FunctionEnd
+
+Function InstallIE6
+	!insertmacro InstallIE 6 "$(IE) 6 $(SP) 1"
+FunctionEnd

setup/DownloadNT4.nsh
@@ -17,8 +17,8 @@ Function NeedsNT4SP6a-wts
 	Call NeedsNT4SP6a
 FunctionEnd
 
-!insertmacro PatchHandler "NT4SP6a-clt" "Windows NT 4.0 $(SP) 6a"        "-u -z" "-n -o"
-!insertmacro PatchHandler "NT4SP6a-wts" "Windows NT 4.0 $(WTS) $(SP) 6a" "-u -z" "-n -o"
+!insertmacro PatchHandler "NT4SP6a-clt" "Windows NT 4.0 $(SP) 6a"        ${PATCH_FLAGS_NT4} ""
+!insertmacro PatchHandler "NT4SP6a-wts" "Windows NT 4.0 $(WTS) $(SP) 6a" ${PATCH_FLAGS_NT4} ""
 
 Function InstallNT4SP6a
 	${If} ${NeedsPatch} NT4SP6a
@@ -75,7 +75,7 @@ FunctionEnd
 Function InstallMSI
 	${If} ${NeedsPatch} MSI
 		${DetailPrint} "$(Installing)$(MSI)..."
-		File "patches\instmsiw.exe"
+		File "patches\redist\instmsiw.exe"
 		!insertmacro ExecWithErrorHandling '$(MSI)' '"$PLUGINSDIR\instmsiw.exe" /c:"msiinst.exe /delayrebootq"'
 	${EndIf}
 FunctionEnd

setup/NT4USB.nsh
@@ -13,25 +13,25 @@ Function InstallNT4USBStack
 
 	; Files
 	SetOutPath "$PROGRAMFILES\Inside Out Networks\Inside Out Networks' USB Peripheral Drivers"
-	File "patches\nt4usb\ReadMe.txt"
+	File "patches\nt4\usb\ReadMe.txt"
 	SetOutPath "$WINDIR"
-	File "patches\nt4usb\ionlicense.txt"
+	File "patches\nt4\usb\ionlicense.txt"
 	SetOutPath "$WINDIR\system32"
-	; File "patches\nt4usb\awusbcfg.exe"
-	File "patches\nt4usb\UsbTray.exe"
-	File "patches\nt4usb\ionusb.exe"
-	File "patches\nt4usb\UsbShare.dll"
+	; File "patches\nt4\usb\awusbcfg.exe"
+	File "patches\nt4\usb\UsbTray.exe"
+	File "patches\nt4\usb\ionusb.exe"
+	File "patches\nt4\usb\UsbShare.dll"
 	SetOutPath "$WINDIR\system32\drivers"
-	File "patches\nt4usb\usbd.sys"
-	File "patches\nt4usb\usbhub.sys"
-	File "patches\nt4usb\vusbd.sys"
+	File "patches\nt4\usb\usbd.sys"
+	File "patches\nt4\usb\usbhub.sys"
+	File "patches\nt4\usb\vusbd.sys"
 
 	; Startup shortcut
 	CreateShortcut "$SMSTARTUP\USB Status Utility.lnk" "$WINDIR\system32\UsbTray.exe"
 
 	; Register services
 	SetOutPath "${RUNONCEDIR}"
-	File "patches\nt4usb\ionsvc.dll"
+	File "patches\nt4\usb\ionsvc.dll"
 	${DetailPrint} "$(Installing)USBD service..."
 	System::Call 'ionsvc::CreateUSBDSrvc(i 0, i 0, s "$WINDIR\system32\usbd.sys") i .r0'
 	${If} $0 != 0
@@ -63,16 +63,16 @@ FunctionEnd
 
 ; TODO: The rest of the files
 
-; File "patches\nt4usb\edgeport.exe"
-; File "patches\nt4usb\edgeser.sys"
-; File "patches\nt4usb\ionflash.exe"
-; File "patches\nt4usb\usbhid.sys"
-; File "patches\nt4usb\usbms.sys"
-; File "patches\nt4usb\UsbMsDll.dll"
-; File "patches\nt4usb\usbprint.sys"
-; File "patches\nt4usb\usbrm.sys"
-; File "patches\nt4usb\vcusbnt4.sys"
-; File "patches\nt4usb\vicam_32.dll"
+; File "patches\nt4\usb\edgeport.exe"
+; File "patches\nt4\usb\edgeser.sys"
+; File "patches\nt4\usb\ionflash.exe"
+; File "patches\nt4\usb\usbhid.sys"
+; File "patches\nt4\usb\usbms.sys"
+; File "patches\nt4\usb\UsbMsDll.dll"
+; File "patches\nt4\usb\usbprint.sys"
+; File "patches\nt4\usb\usbrm.sys"
+; File "patches\nt4\usb\vcusbnt4.sys"
+; File "patches\nt4\usb\vicam_32.dll"
 
 Function InstallNT4USBHID
 	${DetailPrint} "$(Installing)Inside Out Networks USB HID Driver..."
@@ -89,5 +89,5 @@ FunctionEnd
 Function InstallNT4USBEdgeport
 	${DetailPrint} "$(Installing)Inside Out Networks USB to Serial (Edgeport) Drivers..."
 	SetOutPath "$WINDIR\inf"
-	File "patches\nt4usb\rp_mdm.inf"
+	File "patches\nt4\usb\rp_mdm.inf"
 FunctionEnd

setup/patches/.gitignore
@@ -1,0 +1,5 @@
+# Only commit redists
+**/*
+!.gitignore
+!redist
+!redist/**/*

commit 78b9460925e7807c8e96a51b8b72fa2b23f0573d

RunOnce.nsh: Fix a bug that causes the LegacyUpdate installer to not be copied.
This causes a variety of different problems depending on what version of Windows is in use.
On Windows XP x64, Server 2008 x64, and Windows 7 32-bit (which were the three I tried
that the problem was noticable on), the lack of this file causes the kernel to do memory
corruption because the launcher for RunOnce isn't copied, even though it is added into the
Registry. On all three platforms it will routinely cause the ACPI tables to be temporarily
overwritten, as lsass crashes immediately and the kernel won't even BSOD if lsass crashes
in the way that it does. This will make affected systems fail to boot Windows until the
power cable is unplugged from the machine.

Windows 8.1 seems to be unaffected, but some security features in that version of Windows
probably prevent this from happening (or the newer kernel redesign).

At first Kirb and I thought it was a problem with the MinGW toolchain or the new version
of NSIS, but after chasing a gut feeling it turned out to be caused by commit 771c0436d

This one was pretty challenging to figure out because of the lack of diagnostics from
NSIS. The underlying cause was a register conflict.

setup/RunOnce.nsh
@@ -111,12 +111,12 @@ Function PrepareRunOnce
 			; https://web.archive.org/web/20090723061647/http://support.microsoft.com/kb/939857
 			; See also Wine winternl.h
 			!if ${NT4} == 1
-				StrCpy $0 "${RUNONCEDIR}\LegacyUpdateSetup.exe"
+				StrCpy $1 "${RUNONCEDIR}\LegacyUpdateSetup.exe"
 			!else
-				StrCpy $0 "${RUNONCEDIR}\LegacyUpdate.exe"
+				StrCpy $1 "${RUNONCEDIR}\LegacyUpdate.exe"
 			!endif
 
-			!insertmacro RunOnceOverwriteReg Str   HKLM "${REGPATH_SETUP}" "CmdLine" '"$0" /runonce'
+			!insertmacro RunOnceOverwriteReg Str   HKLM "${REGPATH_SETUP}" "CmdLine" '"$1" /runonce'
 			!insertmacro RunOnceOverwriteReg Dword HKLM "${REGPATH_SETUP}" "SetupType" ${SETUP_TYPE_NOREBOOT}
 			WriteRegDword HKLM "${REGPATH_SETUP}" "SetupShutdownRequired" ${SETUP_SHUTDOWN_REBOOT}
 		${EndIf}