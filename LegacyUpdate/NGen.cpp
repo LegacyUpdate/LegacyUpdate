833f1d888f06d0ce9bd11dc3ea4f394abb52284f
Adam Demasi <adam@hbang.com.au>
2025-06-15 14:12:08 +0930

    Don't process the NGen queue on resume
    If it needs to be processed, it will happen automatically on next boot.
    Processing the queue now may not be the best idea, because the user may have
    already clicked to restart, or is about to restart. I believe .NET Framework
    updates usually flag as reboot required anyway.

--- LegacyUpdate/NGen.cpp
+++ LegacyUpdate/NGen.cpp
@@ -31,31 +31,10 @@ STDMETHODIMP PauseResumeNGenQueue(BOOL state) {
 			if (PathFileExists(path)) {
 				hr = Exec(NULL, path, state ? L"queue pause" : L"queue continue", NULL, SW_HIDE, TRUE, NULL);
 
-				if (SUCCEEDED(hr) && state) {
-					// Execute queue in the background
-					hr = Exec(NULL, path, L"executeQueuedItems", NULL, SW_HIDE, FALSE, NULL);
-				}
-
 				if (SUCCEEDED(hr)) {
 					break;
 				}
 			}
-		}
-	}
-
-	if (AtLeastWin8() && state) {
-		// Fire the scheduled tasks
-		static LPWSTR ngenTask = L"\\Microsoft\\Windows\\.NET Framework\\.NET Framework NGEN v4.0.30319";
-
-		for (int i = 0; i < 2; i++) {
-			if (i == 1 && !hasFramework64) {
-				break;
-			}
-
-			WCHAR path[MAX_PATH], args[MAX_PATH];
-			ExpandEnvironmentStrings(L"%SystemRoot%\\System32\\schtasks.exe", path, ARRAYSIZE(path));
-			wsprintf(args, L"/run /tn \"%ls%ls\" /hresult", ngenTask, i == 1 ? L" 64": L"");
-			hr = Exec(NULL, path, args, NULL, SW_HIDE, TRUE, NULL);
 		}
 	}
 
