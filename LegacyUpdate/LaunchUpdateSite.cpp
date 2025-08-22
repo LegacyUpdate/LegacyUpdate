a2b482cf9494a35d9035ea801472e204aab97832
Adam Demasi <adam@hbang.com.au>
2025-08-22 17:22:54 +0930

    Oops, we need COM to use ShellExecute

--- LegacyUpdate/LaunchUpdateSite.cpp
+++ LegacyUpdate/LaunchUpdateSite.cpp
@@ -5,9 +5,18 @@
 // Function signature required by Rundll32.exe.
 EXTERN_C __declspec(dllexport)
 void CALLBACK LaunchUpdateSite(HWND hwnd, HINSTANCE hInstance, LPSTR lpszCmdLine, int nCmdShow) {
+	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+	if (!SUCCEEDED(hr)) {
+		goto end;
+	}
+
 	// This just calls LegacyUpdate.exe now for backwards compatibility.
-	HRESULT hr = StartLauncher(L"/launch", TRUE);
+	hr = StartLauncher(L"/launch", TRUE);
+
+end:
 	if (!SUCCEEDED(hr)) {
 		MessageBox(NULL, GetMessageForHresult(hr), L"Legacy Update", MB_OK | MB_ICONERROR);
 	}
+
+	CoUninitialize();
 }
